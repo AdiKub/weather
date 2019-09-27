c2451856870694e33f359dcbc8111186f886dd3e
adilet <adiletkub@gmail.com>
2019-09-27 17:38:35 +0600

    set default objects for currentWeatherStore and futureWeatherStore. deleted default json files

--- src/store/main/selectors.js
+++ src/store/main/selectors.js
@@ -1,9 +1,50 @@
-import futureWeather from '../../assets/futureWeather.json'
-import currentWeather from '../../assets/currentWeather.json'
-
 export const initialState = {
-    currentWeatherStore: currentWeather,
-    futureWeatherStore: futureWeather,
+    currentWeatherStore: {
+      "coord": {
+        "lon": 74.61,
+        "lat": 42.88
+      },
+      "weather": [
+        {
+          "id": "",
+          "main": "",
+          "description": "",
+          "icon": "01d"
+        }
+      ],
+      "base": "",
+      "main": {
+        "temp": 273
+      },
+      "visibility": 0,
+      "wind": {
+      },
+      "clouds": {
+      },
+      "dt": 0,
+      "sys": {
+      },
+      "timezone": 0,
+      "id": 0,
+      "name": ""
+    },
+    futureWeatherStore: {
+      "cnt": 40,
+      "list": [],
+      "city": {
+        "id": 1528675,
+        "name": "Bishkek",
+        "coord": {
+          "lat": 42.8767,
+          "lon": 74.607
+        },
+        "country": "KG",
+        "population": 0,
+        "timezone": 0,
+        "sunrise": 0,
+        "sunset": 0
+      }
+    },
     currentWeatherFailureStore: {},
     futureWeatherFailureStore: {}
   };
